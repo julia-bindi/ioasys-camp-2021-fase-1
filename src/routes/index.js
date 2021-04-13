d35c823ff65fccb98f15d3a44d1f32a591870d17
julia-bindi <juliabindi@gmail.com>
2021-04-13 15:32:56 -0300

    Função de deletar um usuário

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { auth } = require("./auth.routes");
 const { users } = require("./users.routes");
 const { new_user } = require("./new_user.routes");
 const { new_adm } = require("./new_adm.routes");
+const { delete_user } = require("./delete_use.routes");
 
 module.exports = {
   auth,
   users,
   new_user,
   new_adm,
+  delete_user,
 };
