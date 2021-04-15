9f1bf9c1d4f3cec13f41fc8155ecc87c97df084c
julia-bindi <juliabindi@gmail.com>
2021-04-15 11:27:29 -0300

    Adição da função de deletar um adm

--- src/controllers/deleteUser.controller.js
+++ src/controllers/deleteUser.controller.js
@@ -1,21 +1,21 @@
 const { StatusCodes } = require("http-status-codes");
-const { delete_userService } = require("../services");
+const { deleteUserService } = require("../services");
 const yup = require("yup");
 
 module.exports = {
-    delete_user: async (req, res) => {
+    deleteUser: async (req, res) => {
         try{
             const schema = yup.object().shape({
-                token: yup.string().required(),
                 password: yup.string().required(),
             });
         
             await schema.validate(req.body, {
                 stripUnknown: true,
             });
-        
-            const { token, password } = req.body;
-            const response = await delete_userService.delete_user(token, password);
+            
+            const [scheme, token]  = req.headers.authorization.split(" ");
+            const {password } = req.body;
+            const response = await deleteUserService.deleteUser(token, password);
             return res.status(StatusCodes.OK).json(response);
         }catch (error) {
             console.error(error);
--- src/services/index.js
+++ src/services/index.js
@@ -1,13 +1,17 @@
 const authService = require("./auth");
 const usersService = require("./users");
-const new_userService = require("./oneUser");
-const new_admService = require("./oneUser");
-const delete_userService = require("./oneUser");
+const newUserService = require("./oneUser");
+const newAdmService = require("./oneUser");
+const deleteUserService = require("./oneUser");
+const userUpdateService = require("./oneUser");
+const deleteAdmService = require("./oneUser");
 
 module.exports = {
   authService,
   usersService,
-  new_userService,
-  new_admService,
-  delete_userService,
+  newUserService,
+  newAdmService,
+  deleteUserService,
+  userUpdateService,
+  deleteAdmService
 };
